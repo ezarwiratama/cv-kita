f64c98c598929184ffcd4f874e880678d2a243ff
ezarwiratama <77718063+ezarwiratama@users.noreply.github.com>
2021-01-22 11:03:25 +0700

    menambah tulisan tiga33 di header

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <div className="bg-black">
+          <h1 className="text-9xl text-center font-display font-family:'metal mania' text-white p-40">tiga33</h1>
+        </div>
+    </>
+  )
+  }
+
+export default Header
