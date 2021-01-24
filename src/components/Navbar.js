1716d3dab06828a040cd54afaff758bd21a0ceec
ezarwiratama <ezar.hardin10@gmail.com>
2021-01-24 21:12:07 +0700

    tadi ada yg salah

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,10 @@ function Navbar  () {
         <h4 class="logo">Tiga33</h4>
     </div>
     <ul class="ul">
-            <li className="ul a"><a href="">Home</a></li>
-            <li className="ul a"><a href="">Project</a></li>
-            <li className="ul a"><a href="">Gallery</a></li>
-            <li className="ul a"><a href="">About</a></li>
+            <li className="ul a"><p>Home</p></li>
+            <li className="ul a"><p>Project</p></li>
+            <li className="ul a"><p>Gallery</p></li>
+            <li className="ul a"><p>About</p></li>
     </ul> 
 </nav>
     
