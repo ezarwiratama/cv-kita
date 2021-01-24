004da5d66326aabb7dfbecd00e0d32c24237e581
ezarwiratama <ezar.hardin10@gmail.com>
2021-01-24 22:36:05 +0700

    mengganti warna navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,10 @@ function Navbar  () {
         <h4 class="logo">Tiga33</h4>
     </div>
     <ul class="ul">
-            <li className="ul a"><p>Home</p></li>
-            <li className="ul a"><p>Project</p></li>
-            <li className="ul a"><p>Gallery</p></li>
-            <li className="ul a"><p>About</p></li>
+            <li><p>Home</p></li>
+            <li><p>Project</p></li>
+            <li><p>Gallery</p></li>
+            <li><p>About</p></li>
     </ul> 
 </nav>
     
