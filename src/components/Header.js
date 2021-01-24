af37a7096c41efacac15855d1f479c0c51d793b5
ezarwiratama <ezar.hardin10@gmail.com>
2021-01-24 21:07:46 +0700

    menambah navbar dan mengubah tiga33 header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <div className="header"> 
-            <h1 class="display-1">Tim33</h1>
+            <h1 className="tulisan">Tiga33</h1>
       </div>
     </>
   )
