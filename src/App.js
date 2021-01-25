431de59f123d5e01158a8ba41ca3507a3036ebe4
ezarwiratama <ezar.hardin10@gmail.com>
2021-01-25 16:23:29 +0700

    menambahkan component footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// icon
+import ICFacebook from './icon/045-facebook.svg'
+import ICTwitter from './icon/013-twitter-1.svg'
+import ICWhatsapp from './icon/007-whatsapp.svg'
+import ICInstagram from './icon/034-instagram.svg'
+import ICGithub from './icon/039-github.svg'
+
+export {ICFacebook,ICTwitter,ICWhatsapp,ICInstagram,ICGithub}
+
+
 
 function App() {
   return (
